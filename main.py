d1d528261dcf98e4838451b879a66c3e0b0a5f86
LinusR96 <linus.ronnholm@outlook.com>
2026-04-09 01:35:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import requests
 import time
-print("Bot running...")
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+
+def send(msg):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+    requests.post(url, data={"chat_id": CHAT_ID, "text": msg})
+
+send("🚀 Bot started - Kia Stonic scanner igång!")
+
 while True:
     time.sleep(60)
